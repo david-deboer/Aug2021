0185e91074ee4d9479d1c6cd1402dbd333573e32
David DeBoer <ddeboer@berkeley.edu>
2020-09-18 11:40:20 -0700

    redid lots

--- mtgplan.py
+++ mtgplan.py
@@ -1,7 +1,10 @@
 import json
+from argparse import Namespace
+import os
+
 
 with open('hera_mtg_planning.json', 'r') as fp:
-    mtg = json.load(fp)
+    mtg_availability = json.load(fp)
 
 meetings = ['All-hands meeting', 'Analog', 'Pspec', 'pyuvsim/pyradiosky alternating',
             'Commissioning', 'Correlator', 'Site', 'pyuvdata', 'NRAO',
@@ -10,47 +13,76 @@ meetings = ['All-hands meeting', 'Analog', 'Pspec', 'pyuvsim/pyradiosky alternat
 
 dow = {'Mon': '1', 'Tues': '2', 'Wed': '3', 'Thurs': '4', 'Fri': '5'}
 
-num_all = len(list(mtg.keys()))
-all_pop = {}
-mtg_by_mtg = {}
-all_mtg_by_mtg = {}
-essential = {}
-all_essential = {}
-for pn in mtg.keys():
-    for avail in mtg[pn]['available']:
-        all_pop.setdefault(avail, [])
-        all_pop[avail].append(pn)
-    for this_mtg in mtg[pn]['meetings']:
-        all_mtg_by_mtg.setdefault(this_mtg, [])
-        all_mtg_by_mtg[this_mtg].append(pn)
-        mtg_by_mtg.setdefault(this_mtg, {})
-        for avail in mtg[pn]['available']:
-            mtg_by_mtg[this_mtg].setdefault(avail, [])
-            mtg_by_mtg[this_mtg][avail].append(pn)
-    for this_mtg in mtg[pn]['x']:
-        all_essential.setdefault(this_mtg, [])
-        all_essential[this_mtg].append(pn)
-        essential.setdefault(this_mtg, {})
-        for avail in mtg[pn]['available']:
-            essential[this_mtg].setdefault(avail, [])
-            essential[this_mtg][avail].append(pn)
+MD = Namespace(pop={}, mtg={}, all_mtg={}, x={}, all_x={})
+
+for person in mtg_availability.keys():
+    for avail in mtg_availability[person]['available']:
+        MD.pop.setdefault(avail, [])
+        MD.pop[avail].append(person)
+    for this_mtg in mtg_availability[person]['meetings']:
+        MD.all_mtg.setdefault(this_mtg, [])
+        MD.all_mtg[this_mtg].append(person)
+        MD.mtg.setdefault(this_mtg, {})
+        for avail in mtg_availability[person]['available']:
+            MD.mtg[this_mtg].setdefault(avail, [])
+            MD.mtg[this_mtg][avail].append(person)
+    for this_mtg in mtg_availability[person]['x']:
+        MD.all_x.setdefault(this_mtg, [])
+        MD.all_x[this_mtg].append(person)
+        MD.x.setdefault(this_mtg, {})
+        for avail in mtg_availability[person]['available']:
+            MD.x[this_mtg].setdefault(avail, [])
+            MD.x[this_mtg][avail].append(person)
+
+csv_file = 'results.csv'
+if os.path.exists(csv_file):
+    os.remove(csv_file)
 
 
-def view(this_one=mtg_by_mtg['All-hands meeting'], Nplan=-1, include_names=True, cull=0):
+def view(mtype='mtg', meeting='All-hands meeting',
+         header=True, names='not-present', show=0, csv=True):
     ranked = {}
-    for key, val in this_one.items():
+    if mtype == 'pop':
+        this = MD.pop
+        all = list(mtg_availability.keys())
+    else:
+        this = getattr(MD, mtype)[meeting]
+        all = getattr(MD, 'all_'+mtype)[meeting]
+    hdr = "{}-{}:  {}".format(mtype, meeting, ', '.join(all))
+    if names:
+        hdr += '\n{}<{}>'.format(24*' ', names)
+    if header:
+        print(hdr)
+    for key, val in this.items():
         d, h = key.split()
         th = [int(x) for x in h.split(':')]
         this_key = "{:02d}_{}_{:02d}:{:02d}_{}".format(len(val), dow[d], th[0], th[1], key)
         ranked[this_key] = val
-
+    this_len = len(all)
+    if csv:
+        fp = open(csv_file, 'a')
+        # print('{},{},{},{}'.format(mtype, meeting, names, ','.join(all)), file=fp)
     for ordrk in sorted(list(ranked.keys()), reverse=True):
         n, _a, _b, sch = ordrk.split('_')
-        if int(n) < cull:
+        if int(n) < show:
             continue
-        if Nplan > 0:
-            print('{} / {:02d} {:14s}'.format(n, Nplan, sch))
-        else:
-            print('{} {:14s}'.format(n, sch))
-        if include_names:
+        csv_row = [mtype, meeting, n, this_len, sch.split()[0], sch.split()[1]]
+        print('{} / {:02d} {:14s}'.format(n, this_len, sch), end='')
+        if names == 'present':
             print('  {}'.format(', '.join(ranked[ordrk])))
+            csv_row += ranked[ordrk]
+        elif names == 'not-present':
+            np = []
+            for nm in all:
+                if nm not in ranked[ordrk]:
+                    np.append(nm)
+            print('  {}'.format(', '.join(np)))
+            csv_row += np
+        else:
+            print()
+        if csv:
+            row = ','.join([str(x) for x in csv_row])
+            print('{}'.format(row), file=fp)
+    if csv:
+        fp.close()
+    return ranked
--- quick_script.py
+++ quick_script.py
@@ -1,20 +1,18 @@
 import mtgplan as mp
 # See list in mtgplan
-incl_names = True
+incl_names = 'not-present'  # vs 'present' or False
 meeting = "All-hands meeting"  # see meetings in mtgplan.py
 
 print("VIABLE (subjective; 'x' field in hera_mtg_planning.json)")
-print(', '.join(mp.all_essential[meeting]))
 show_down_to = 12  # cut-off number
-mp.view(mp.essential[meeting], len(mp.all_essential[meeting]), incl_names, show_down_to)
+x = mp.view('x', meeting, header=True, names=incl_names, show=show_down_to)
 
 print("-------------------------------------------")
 print("SELF-SELECTED (from googlesheet; 'meetings' in hera_mtg_planning.json)")
-print(', '.join(mp.all_mtg_by_mtg[meeting]))
 show_down_to = 15  # cut-off number
-mp.view(mp.mtg_by_mtg[meeting], len(mp.all_mtg_by_mtg[meeting]), incl_names, show_down_to)
+m = mp.view('mtg', meeting, header=True, names=incl_names, show=show_down_to)
 
 print("-------------------------------------------")
 print("ALL (all from whenisgood)")
 show_down_to = 23  # cut-off number
-mp.view(mp.all_pop, mp.num_all, incl_names, show_down_to)
+p = mp.view('pop', meeting, header=False, names=incl_names, show=show_down_to)
