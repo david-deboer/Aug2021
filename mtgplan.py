201ffeec8fdb40f3a2a95f47fd50eb7597a5cda6
David DeBoer <ddeboer@berkeley.edu>
2020-09-22 21:54:29 -0700

    made a class

--- mtgplan.py
+++ mtgplan.py
@@ -1,88 +1,102 @@
 import json
-from argparse import Namespace
-import os
 
 
-with open('hera_mtg_planning.json', 'r') as fp:
-    mtg_availability = json.load(fp)
+class MeetingPlanner:
+    meetings = ['All-hands meeting', 'Analog', 'Pspec',
+                'pyuvsim/pyradiosky alternating', 'Commissioning', 'Correlator',
+                'Site', 'pyuvdata', 'NRAO', 'Analysis/Quality Metrics alternating',
+                'Imaging (biweekly)', 'Validation', 'Machine learning',
+                'Theory Meets Data', 'Instrument Modeling']
+    dow = {'Mon': '1', 'Tues': '2', 'Wed': '3', 'Thurs': '4', 'Fri': '5'}
+    all_mtypes = ['all', 'mtg', 'x']
 
-meetings = ['All-hands meeting', 'Analog', 'Pspec', 'pyuvsim/pyradiosky alternating',
-            'Commissioning', 'Correlator', 'Site', 'pyuvdata', 'NRAO',
-            'Analysis/Quality Metrics alternating', 'Imaging (biweekly)', 'Validation',
-            'Machine learning', 'Theory Meets Data', 'Instrument Modeling']
+    def __init__(self, fn='hera_mtg_planning.json'):
+        self.fn = fn
+        for mtype in self.all_mtypes:
+            setattr(self, mtype, {})
+            setattr(self, 'all_{}'.format(mtype), {})
+        self.csv_fp = None
 
-dow = {'Mon': '1', 'Tues': '2', 'Wed': '3', 'Thurs': '4', 'Fri': '5'}
+    def setup(self):
+        with open(self.fn, 'r') as fp:
+            self.mtg_availability = json.load(fp)
+        for person in self.mtg_availability.keys():
+            for avail in self.mtg_availability[person]['available']:
+                self.all.setdefault(avail, [])
+                self.all[avail].append(person)
+            for this_mtg in self.mtg_availability[person]['meetings']:
+                self.all_mtg.setdefault(this_mtg, [])
+                self.all_mtg[this_mtg].append(person)
+                self.mtg.setdefault(this_mtg, {})
+                for avail in self.mtg_availability[person]['available']:
+                    self.mtg[this_mtg].setdefault(avail, [])
+                    self.mtg[this_mtg][avail].append(person)
+            for this_mtg in self.mtg_availability[person]['x']:
+                self.all_x.setdefault(this_mtg, [])
+                self.all_x[this_mtg].append(person)
+                self.x.setdefault(this_mtg, {})
+                for avail in self.mtg_availability[person]['available']:
+                    self.x[this_mtg].setdefault(avail, [])
+                    self.x[this_mtg][avail].append(person)
 
-MD = Namespace(pop={}, mtg={}, all_mtg={}, x={}, all_x={})
-
-for person in mtg_availability.keys():
-    for avail in mtg_availability[person]['available']:
-        MD.pop.setdefault(avail, [])
-        MD.pop[avail].append(person)
-    for this_mtg in mtg_availability[person]['meetings']:
-        MD.all_mtg.setdefault(this_mtg, [])
-        MD.all_mtg[this_mtg].append(person)
-        MD.mtg.setdefault(this_mtg, {})
-        for avail in mtg_availability[person]['available']:
-            MD.mtg[this_mtg].setdefault(avail, [])
-            MD.mtg[this_mtg][avail].append(person)
-    for this_mtg in mtg_availability[person]['x']:
-        MD.all_x.setdefault(this_mtg, [])
-        MD.all_x[this_mtg].append(person)
-        MD.x.setdefault(this_mtg, {})
-        for avail in mtg_availability[person]['available']:
-            MD.x[this_mtg].setdefault(avail, [])
-            MD.x[this_mtg][avail].append(person)
-
-csv_file = 'results.csv'
-if os.path.exists(csv_file):
-    os.remove(csv_file)
-
-
-def view(mtype='mtg', meeting='All-hands meeting',
-         header=True, names='not-present', show=0, csv=True):
-    ranked = {}
-    if mtype == 'pop':
-        this = MD.pop
-        all = list(mtg_availability.keys())
-    else:
-        this = getattr(MD, mtype)[meeting]
-        all = getattr(MD, 'all_'+mtype)[meeting]
-    hdr = "{}-{}:  {}".format(mtype, meeting, ', '.join(all))
-    if names:
-        hdr += '\n{}<{}>'.format(24*' ', names)
-    if header:
-        print(hdr)
-    for key, val in this.items():
-        d, h = key.split()
-        th = [int(x) for x in h.split(':')]
-        this_key = "{:02d}_{}_{:02d}:{:02d}_{}".format(len(val), dow[d], th[0], th[1], key)
-        ranked[this_key] = val
-    this_len = len(all)
-    if csv:
-        fp = open(csv_file, 'a')
-        # print('{},{},{},{}'.format(mtype, meeting, names, ','.join(all)), file=fp)
-    for ordrk in sorted(list(ranked.keys()), reverse=True):
-        n, _a, _b, sch = ordrk.split('_')
-        if int(n) < show:
-            continue
-        csv_row = [mtype, meeting, n, this_len, sch.split()[0], sch.split()[1]]
-        print('{} / {:02d} {:14s}'.format(n, this_len, sch), end='')
-        if names == 'present':
-            print('  {}'.format(', '.join(ranked[ordrk])))
-            csv_row += ranked[ordrk]
-        elif names == 'not-present':
-            np = []
-            for nm in all:
-                if nm not in ranked[ordrk]:
-                    np.append(nm)
-            print('  {}'.format(', '.join(np)))
-            csv_row += np
+    def reset_file(self, meeting):
+        if self.csv_fp is None:
+            meeting = meeting.replace('/', '')
+            meeting = meeting.replace(' ', '')
+            meeting = meeting.replace('(', '')
+            meeting = meeting.replace(')', '')
+            meeting = meeting.replace('-', '')
+            self.mcsv = "{}.csv".format(meeting)
+            self.csv_fp = open(self.mcsv, 'w')
+            print("mtype,meeting,available,total,day,hour,not-present", file=self.csv_fp)
         else:
-            print()
-        if csv:
-            row = ','.join([str(x) for x in csv_row])
-            print('{}'.format(row), file=fp)
-    if csv:
-        fp.close()
-    return ranked
+            self.csv_fp.close()
+            print("Writing {}".format(self.mcsv))
+            self.csv_fp = None
+
+    def view(self, mtype='mtg', meeting='All-hands meeting',
+             header=True, names='not-present', show=0, csv=True):
+        if meeting not in self.meetings:
+            raise ValueError("{} not valid meeting".format(meeting))
+        ranked = {}
+        if mtype == 'all':
+            this = self.all
+            all = list(self.mtg_availability.keys())
+        else:
+            this = getattr(self, mtype)[meeting]
+            all = getattr(self, 'all_'+mtype)[meeting]
+        hdr = "{}-{}:  {}".format(mtype, meeting, ', '.join(all))
+        if names:
+            hdr += '\n{}<{}>'.format(24*' ', names)
+        if header:
+            print(hdr)
+        for key, val in this.items():
+            d, h = key.split()
+            th = [int(x) for x in h.split(':')]
+            this_key = "{:02d}_{}_{:02d}:{:02d}_{}".format(len(val), self.dow[d], th[0], th[1], key)
+            ranked[this_key] = val
+        this_len = len(all)
+        # if csv:
+        #     print('{},{},{},{}'.format(mtype, meeting, names, ','.join(all)), file=self.csv_fp)
+        for ordrk in sorted(list(ranked.keys()), reverse=True):
+            n, _a, _b, sch = ordrk.split('_')
+            if int(n) < show:
+                continue
+            csv_row = [mtype, meeting, n, this_len, sch.split()[0], sch.split()[1]]
+            print('{} / {:02d} {:14s}'.format(n, this_len, sch), end='')
+            if names == 'present':
+                print('  {}'.format(', '.join(ranked[ordrk])))
+                csv_row += ranked[ordrk]
+            elif names == 'not-present':
+                np = []
+                for nm in all:
+                    if nm not in ranked[ordrk]:
+                        np.append(nm)
+                print('  {}'.format(', '.join(np)))
+                csv_row += np
+            else:
+                print()
+            if csv:
+                row = ','.join([str(x) for x in csv_row])
+                print('{}'.format(row), file=self.csv_fp)
+        return ranked
--- quick_script.py
+++ quick_script.py
@@ -1,22 +1,49 @@
-import mtgplan as mp
+#! /usr/bin/env python
+import mtgplan
+import argparse
 
-print(mp.meetings)
+ap = argparse.ArgumentParser()
+ap.add_argument('meeting', help="Name of meeting unless None", nargs='?', default=None)
+ap.add_argument('--include-names', dest='incl_names', help="not-present, present or none",
+                default='not-present')
+ap.add_argument('--csv', help="Write csv file", action='store_true')
+ap.add_argument('--mtype', help="mtypes", default="x,mtg,all")
+args = ap.parse_args()
 
-incl_names = 'not-present'  # vs 'present' or False
-meeting = "All-hands meeting"  # see meetings in mtgplan.py
+mp = mtgplan.MeetingPlanner()
+mp.setup()
+print("=====================================================")
+print("=====================================================")
 
-print("VIABLE (subjective; 'x' field in hera_mtg_planning.json)")
-show_down_to = 12  # cut-off number
-x = mp.view('x', meeting, header=True, names=incl_names, show=show_down_to)
+show = {'All-hands meeting':
+        {'x': [12, True], 'mtg': [12, True], 'all': [23, False]},
+        'Other':
+        {'x': [0, True], 'mtg': [0, True], 'all': [23, True]}
+        }
 
-print("-------------------------------------------")
-print("SELF-SELECTED (from googlesheet; 'meetings' in hera_mtg_planning.json)")
-show_down_to = 15  # cut-off number
-m = mp.view('mtg', meeting, header=True, names=incl_names, show=show_down_to)
+mtypes = {'x': "VIABLE (subjective; 'x' field in hera_mtg_planning.json)",
+          'mtg': "SELF-SELECTED (from googlesheet; 'meetings' in hera_mtg_planning.json)",
+          'all': "ALL (all from whenisgood)"}
+args.mtype = args.mtype.split(',')
 
-print("-------------------------------------------")
-print("ALL (all from whenisgood)")
-show_down_to = 23  # cut-off number
-p = mp.view('pop', meeting, header=False, names=incl_names, show=show_down_to)
+if args.meeting is None:
+    meeting_set = mp.meetings
+else:
+    meeting_set = args.meeting.split(',')
 
-print("writing results.csv")
+for meeting in meeting_set:
+    print(meeting)
+    if args.csv:
+        mp.reset_file(meeting)
+
+    for mtype in args.mtype:
+        print(mtypes[mtype])
+        show_down_to = show[meeting][mtype][0]\
+            if meeting in show.keys() else show['Other'][mtype][0]
+        show_header = show[meeting][mtype][1]\
+            if meeting in show.keys() else show['Other'][mtype][1]
+        x = mp.view(mtype, meeting, header=show_header, names=args.incl_names, show=show_down_to)
+        print("-------------------------------------------")
+
+    if args.csv:
+        mp.reset_file(meeting)
