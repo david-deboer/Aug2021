407e36858745b8966571e85166daa14716514c16
David DeBoer <ddeboer@berkeley.edu>
2020-09-23 22:02:34 -0700

    major rewrite draft

--- mtgplan.py
+++ mtgplan.py
@@ -1,15 +1,19 @@
 import json
-DOW = {'Mon': '1', 'Tues': '2', 'Wed': '3', 'Thurs': '4', 'Fri': '5'}
+DOW = {'Mon': '5', 'Tues': '4', 'Wed': '3', 'Thurs': '2', 'Fri': '1'}
 
 
 class MeetingPlanner:
 
     def __init__(self, fn='hera_mtg_planning.json', info_fn="meeting_info.json"):
+        """
+        Reads info and sets up empty dictionaries
+        """
         self.fn = fn
         with open(info_fn, 'r') as fp:
             self.info = json.load(fp)
         self.meetings = {}
         self.planner = {}
+        self.ranked = {}
         for mtg in self.info['meetings']:
             self.meetings[mtg] = {}
             self.planner[mtg] = {}
@@ -19,6 +23,13 @@ class MeetingPlanner:
         self.csv_fp = None
 
     def setup(self):
+        """
+        Produces the following class nested dictionaries:
+            self.team
+            self.meetings
+            self.planner
+            self.ranked
+        """
         with open(self.fn, 'r') as fp:
             self.team = json.load(fp)
         for person, mtg_info in self.team.items():
@@ -34,8 +45,19 @@ class MeetingPlanner:
                         if person_in_mtggrp:
                             self.planner[mtg][grp].setdefault(available, [])
                             self.planner[mtg][grp][available].append(person)
+        for mtg in self.info['meetings']:
+            self.ranked[mtg] = {}
+            for grp in self.info['groups']:
+                self.ranked[mtg][grp] = {}
+                for available, p_list in self.planner[mtg][grp].items():
+                    dn, tm = available.split()
+                    hr, mn = [99-int(x) for x in tm.split(':')]  # the 99/DOW are for reverse sort
+                    key = "{:03d}-{}-{:02d}-{:02d}".format(len(p_list), DOW[dn], hr, mn)
+                    self.ranked[mtg][grp][key] = available
 
-    def reset_file(self, meeting):
+    def handle_file(self, meeting):
+        if meeting not in self.info['meetings']:
+            raise ValueError("{} not valid meeting".format(meeting))
         if self.csv_fp is None:
             meeting = meeting.replace('/', '')
             meeting = meeting.replace(' ', '')
@@ -50,45 +72,34 @@ class MeetingPlanner:
             print("Writing {}".format(self.mcsv))
             self.csv_fp = None
 
-    def view(self, group='meetings', meeting='All-hands meeting',
-             header=True, names='not-present', show=0, csv=True):
+    def view(self, meeting, group, names_shown_as='not-present', csv=True):
         if meeting not in self.info['meetings']:
             raise ValueError("{} not valid meeting".format(meeting))
         if group not in self.info['groups']:
-            raise ValueError("{} not valid group".format(group))
-        ranked = {}
-        hdr = "{}-{}:  {}".format(group, meeting, ', '.join(self.meetings[meeting][group]))
-        if names:
-            hdr += '\n{}<{}>'.format(24*' ', names)
-        if header:
-            print(hdr)
-        for key, val in this.items():
-            d, h = key.split()
-            th = [int(x) for x in h.split(':')]
-            this_key = "{:02d}_{}_{:02d}:{:02d}_{}".format(len(val), DOW[d], th[0], th[1], key)
-            ranked[this_key] = val
-        this_len = len(all)
-        # if csv:
-        #     print('{},{},{},{}'.format(group, meeting, names, ','.join(all)), file=self.csv_fp)
-        for ordrk in sorted(list(ranked.keys()), reverse=True):
-            n, _a, _b, sch = ordrk.split('_')
-            if int(n) < show:
-                continue
-            csv_row = [group, meeting, n, this_len, sch.split()[0], sch.split()[1]]
-            print('{} / {:02d} {:14s}'.format(n, this_len, sch), end='')
-            if names == 'present':
-                print('  {}'.format(', '.join(ranked[ordrk])))
-                csv_row += ranked[ordrk]
-            elif names == 'not-present':
-                np = []
-                for nm in all:
-                    if nm not in ranked[ordrk]:
-                        np.append(nm)
-                print('  {}'.format(', '.join(np)))
-                csv_row += np
-            else:
-                print()
+            raise ValueError("{} not valid group".format(meeting))
+        if csv and self.csv_fp is None:
+            print("No csv file open - not writing")
+            csv = False
+        ranked_order = sorted(list(self.ranked[meeting][group].keys()), reverse=True)
+        full_group = self.meetings[meeting][group]
+        for ro in ranked_order:
+            this_time = self.ranked[meeting][group][ro]
+            available = self.planner[meeting][group][this_time]
+            _x = "{:>3s} /{:>3s}".format(str(len(available)), str(len(full_group)))
+            _t = "{:5s} {:>5s}".format(this_time.split()[0], this_time.split()[1])
+            names = get_name_list(available, full_group, names_shown_as)
+            print('{}  {:8s}  {}'.format(_t, _x, ', '.join(names)))
             if csv:
-                row = ','.join([str(x) for x in csv_row])
-                print('{}'.format(row), file=self.csv_fp)
-        return ranked
+                print("{},{},{},{},{},{},{}".format(meeting, group,
+                                                    this_time.split()[0], this_time.split()[1],
+                                                    len(available), len(full_group)), file=self.csv_fp)  # noqa
+
+
+def get_name_list(this_list, full_list, names_shown_as):
+    if names_shown_as == 'present':
+        return this_list
+    names_list = []
+    for nm in full_list:
+        if nm not in this_list:
+            names_list.append(nm)
+    return names_list
---
+++ plan_meeting.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+import mtgplan
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('meeting', help="Name of meeting unless None which does all",
+                nargs='?', default=None)
+ap.add_argument('--include-names', dest='incl_names', help="not-present or present",
+                default='not-present')
+ap.add_argument('--csv', help="Write csv file", action='store_true')
+ap.add_argument('--groups', help="groups to use", default="x,meetings,all")
+args = ap.parse_args()
+
+mp = mtgplan.MeetingPlanner()
+mp.setup()
+print("=====================================================")
+print("=====================================================")
+
+group_desc = {'x': "VIABLE (subjective; 'x' field in hera_mtg_planning.json)",
+              'meetings': "SELF-SELECTED (from googlesheet; 'meetings' in hera_mtg_planning.json)",
+              'all': "ALL (all from whenisgood)"}
+args.groups = args.groups.split(',')
+
+if args.meeting is None:
+    meeting_set = mp.info['meetings']
+else:
+    meeting_set = args.meeting.split(',')
+
+for meeting in meeting_set:
+    print(meeting)
+    if args.csv:
+        mp.reset_file(meeting)
+
+    for group in args.groups:
+        print(group_desc[group])
+        x = mp.view(meeting, group, names_shown_as=args.incl_names, csv=args.csv)
+        print("-------------------------------------------")
+
+    if args.csv:
+        mp.reset_file(meeting)
